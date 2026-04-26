08b517919fb7c137a50af31fb1539538dedc9d4a
Amr_Hesham <amrheshammacc@gmail.com>
2026-04-26 22:04:57 +0300

    fix: test why not getting a response

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { PROCESS_EXIT_CODE } from './config/constants.js';
 const startServer = async (): Promise<void> => {
   const app = await buildApp();
 
-  await app.listen({ port: app.config.PORT, host: '127.0.0.1' });
+  await app.listen({ port: app.config.PORT, host: '0.0.0.0' });
 };
 
 startServer().catch((err: unknown) => {
